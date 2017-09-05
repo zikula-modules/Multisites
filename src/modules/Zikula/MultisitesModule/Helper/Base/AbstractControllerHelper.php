d83f5e917e15b28230d6beb5efaa765e58051d2c
Guite CI <support@guite.de>
2017-09-05 17:53:45 +0200

    Regenerated using latest generator

--- src/modules/Zikula/MultisitesModule/Helper/Base/AbstractControllerHelper.php
+++ src/modules/Zikula/MultisitesModule/Helper/Base/AbstractControllerHelper.php
@@ -219,6 +219,7 @@ abstract class AbstractControllerHelper
             }
         }
         $sortableColumns->setOrderBy($sortableColumns->getColumn($sort), strtoupper($sortdir));
+        $resultsPerPage = $templateParameters['num'];
     
         $urlParameters = $templateParameters;
         foreach ($urlParameters as $parameterName => $parameterValue) {
