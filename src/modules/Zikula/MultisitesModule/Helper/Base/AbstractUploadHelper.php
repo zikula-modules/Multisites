3d32a3b15459439573097bb72a0e947632f901b7
Guite CI <support@guite.de>
2018-08-07 12:37:30 +0200

    Regenerated using latest generator

--- src/modules/Zikula/MultisitesModule/Helper/Base/AbstractUploadHelper.php
+++ src/modules/Zikula/MultisitesModule/Helper/Base/AbstractUploadHelper.php
@@ -503,7 +503,7 @@ abstract class AbstractUploadHelper
         if (is_array($entity[$fieldName]) && isset($entity[$fieldName][$fieldName])) {
             $entity[$fieldName] = $entity[$fieldName][$fieldName];
         }
-        $filePath = is_object($entity[$fieldName]) ? $entity[$fieldName]->getPathname() : $entity[$fieldName];
+        $filePath = $entity[$fieldName] instanceof File ? $entity[$fieldName]->getPathname() : $entity[$fieldName];
         if (file_exists($filePath) && !unlink($filePath)) {
             return false;
         }
