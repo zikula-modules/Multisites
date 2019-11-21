f10f79053800ce85c944b24e5cb80705948aefc0
Guite CI <support@guite.de>
2019-11-21 06:51:32 +0100

    Regenerated using latest generator

--- src/modules/Zikula/MultisitesModule/Resources/public/js/ZikulaMultisitesModule.js
+++ src/modules/Zikula/MultisitesModule/Resources/public/js/ZikulaMultisitesModule.js
@@ -44,21 +44,20 @@ function zikulaMultisitesToggleFlag(objectType, fieldName, itemId) {
             ot: objectType,
             field: fieldName,
             id: itemId
-        },
-        success: function (data) {
-            var idSuffix;
-            var toggleLink;
-
-            idSuffix = zikulaMultisitesCapitaliseFirstLetter(fieldName) + itemId;
-            toggleLink = jQuery('#toggle' + idSuffix);
-
-            /*if (data.message) {
-                zikulaMultisitesSimpleAlert(toggleLink, Translator.__('Success'), data.message, 'toggle' + idSuffix + 'DoneAlert', 'success');
-            }*/
-
-            toggleLink.find('.fa-check').toggleClass('hidden', true !== data.state);
-            toggleLink.find('.fa-times').toggleClass('hidden', true === data.state);
         }
+    }).done(function (data) {
+        var idSuffix;
+        var toggleLink;
+
+        idSuffix = zikulaMultisitesCapitaliseFirstLetter(fieldName) + itemId;
+        toggleLink = jQuery('#toggle' + idSuffix);
+
+        /*if (data.message) {
+            zikulaMultisitesSimpleAlert(toggleLink, Translator.__('Success'), data.message, 'toggle' + idSuffix + 'DoneAlert', 'success');
+        }*/
+
+        toggleLink.find('.fa-check').toggleClass('hidden', true !== data.state);
+        toggleLink.find('.fa-times').toggleClass('hidden', true === data.state);
     });
 }
 
